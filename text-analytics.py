aeb23b8143472e3b140d0f77ec03616ef97e6721
pQbas <percybrayan18@gmail.com>
2024-12-04 22:49:23 -0500

    update code with more examples

--- text-analytics.py
+++ text-analytics.py
@@ -1,21 +1,48 @@
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
-def analyze_sentiment(text):
-    documents = [text]
-    response = client.analyze_sentiment(documents=documents)[0]
+def analyze_sentiment(texts):
+    print('> analyze sentiment ...')
+    response = client.analyze_sentiment(documents=texts)
     
-    print(f"Sentiment: {response.sentiment}")
-    print(f"Positive score: {response.confidence_scores.positive}")
-    print(f"Neutral score: {response.confidence_scores.neutral}")
-    print(f"Negative score: {response.confidence_scores.negative}")
+    for document in response:
+        print(f"Sentiment: {document.sentiment}")
+        print(f"Positive score: {document.confidence_scores.positive}")
+        print(f"Neutral score: {document.confidence_scores.neutral}")
+        print(f"Negative score: {document.confidence_scores.negative}")
+        print("-----")
+
+def extract_key_phrases(texts):
+    print('> extracting key phrases ...')
+    response = client.extract_key_phrases(documents=texts)
+    
+    for document in response:
+        print(f"Key phrases: {document.key_phrases}")
+        print("-----")
+
+
+def detect_language(texts):
+    print('> detecting language ...')
+    response = client.detect_language(documents=texts)
+    
+    for document in response:
+        print(f"Detected language: {document.primary_language.name}")
+        print(f"Language confidence score: {document.primary_language.confidence_score}")
+        print("-----")
 
 
 if __name__ == '__main__':
-    endpoint = "<your-endpoint>"
-    api_key = "<your-api-key>"
+    endpoint = 'replace with your endpoint service'
+    api_key = 'replace with your api-key' 
 
     client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(api_key))
 
-    analyze_sentiment("I love using Azure Cognitive Services!")
+    texts = [
+        "I love using Azure Cognitive Services. It's fantastic!",
+        "The weather is nice today."
+    ]
+
+    analyze_sentiment(texts)
+    extract_key_phrases(texts)
+    detect_language(texts)
 
